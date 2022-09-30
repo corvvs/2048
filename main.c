cd2a20f1d590cfb336e2be14c79c3e69e96dade7
nori <nori623p@gmail.com>
2022-09-30 14:29:55 +0200

    feat: 🎸 ncursesの使いそうな部分を追加

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <ncurses.h>
+#include <stdbool.h>
+
+// エラーハンドリング
+void init_ncurses()
+{
+	initscr();
+	timeout(-1);
+	noecho();
+	curs_set(0);
+}
+
+int main()
+{
+	init_ncurses();
+	while (true) {
+		int c = getch();
+		printw("[%c]", c);
+	}
+	endwin();
+}
+
+// while (true) {
+// 	t_moved moved;
+
+// 	put_new_value(g_board);
+// 	print_board(g_board);
+// 	create_moved_board_for_every_direction(g_board, &moved);
+// 	if (!moved.can_move) {
+// 		break;
+// 	}
+// 	int direction = Getchar(moved.movable_set);
+// 	if (direction == EOF || direction == ESC) {
+// 		break;
+// 	}
+// 	g_board = moved.boards[direction];
+// 	print_board(g_board);
+// 	if (!is_victory(g_board)) {
+// 		continue;
+// 	}
+// 	int res = display_dialogue();
+// 	if (res == end) {
+// 		break;
+// 	}
+// }
