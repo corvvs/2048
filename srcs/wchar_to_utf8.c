849855ca8cea3e700ceba622e69b39e7c2747341
nori <nori623p@gmail.com>
2022-10-03 21:14:54 +0200

    refactor: 💡 ビット演算減らした

--- srcs/wchar_to_utf8.c
+++ srcs/wchar_to_utf8.c
@@ -3,29 +3,37 @@
 
 #include "ft_utf8.h"
 
+typedef union {
+	uint32_t dword;
+	uint8_t  byte[4];
+} t_dword;
+
 uint32_t encode_2bytes_utf8(wchar_t wc)
 {
-	t_utf8_2 u      = {wc};
-	uint32_t first  = 0b110 << 5 | u.second_lower << 2 | u.first_upper;
-	uint32_t second = 0b10 << 6 | u.first_lower;
-	return first | second << 8;
+	t_utf8_2 u       = {wc};
+	t_dword  encoded = {0};
+	encoded.byte[0]  = 0b110 << 5 | u.second_lower << 2 | u.first_upper;
+	encoded.byte[1]  = 0b10 << 6 | u.first_lower;
+	return encoded.dword;
 }
 
 uint32_t encode_3bytes_utf8(wchar_t wc)
 {
-	t_utf8_3 u      = {wc};
-	uint32_t first  = 0b1110 << 4 | u.second_upper;
-	uint32_t second = 0b10 << 6 | u.second_lower << 2 | u.first_upper;
-	uint32_t third  = 0b10 << 6 | u.first_lower;
-	return first | second << 8 | third << 16;
+	t_utf8_3 u       = {wc};
+	t_dword  encoded = {0};
+	encoded.byte[0]  = 0b1110 << 4 | u.second_upper;
+	encoded.byte[1]  = 0b10 << 6 | u.second_lower << 2 | u.first_upper;
+	encoded.byte[2]  = 0b10 << 6 | u.first_lower;
+	return encoded.dword;
 }
 
 uint32_t encode_4bytes_utf8(wchar_t wc)
 {
-	t_utf8_4 u      = {wc};
-	uint32_t first  = 0b11110 << 3 | u.third_upper;
-	uint32_t second = 0b10 << 6 | u.third_lower << 4 | u.second_upper;
-	uint32_t third  = 0b10 << 6 | u.second_lower << 2 | u.first_upper;
-	uint32_t fourth = 0b10 << 6 | u.first_lower;
-	return first | second << 8 | third << 16 | fourth << 24;
+	t_utf8_4 u       = {wc};
+	t_dword  encoded = {0};
+	encoded.byte[0]  = 0b11110 << 3 | u.third_upper;
+	encoded.byte[1]  = 0b10 << 6 | u.third_lower << 4 | u.second_upper;
+	encoded.byte[2]  = 0b10 << 6 | u.second_lower << 2 | u.first_upper;
+	encoded.byte[3]  = 0b10 << 6 | u.first_lower;
+	return encoded.dword;
 }
